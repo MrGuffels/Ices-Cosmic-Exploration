ccf4dbf6e2497fa191875b711e0ccfb38f2e4a92
Leontopodium Nivale <103514618+LeontopodiumNivale14@users.noreply.github.com>
2026-02-22 11:24:58 -0500

    Made Randomize waypoint Radius default 1.0

--- ICE/ConfigFiles/Config_Misc.cs
+++ ICE/ConfigFiles/Config_Misc.cs
@@ -21,7 +21,7 @@ public partial class Config
     public bool PersonalReturnSpot { get; set; } = false;
     public bool ClosestNodeSelection { get; set; } = false;
     public bool RandomizeWaypoints { get; set; } = false;
-    public float RandomizeWaypointsRadius { get; set; } = 1.5f;
+    public float RandomizeWaypointsRadius { get; set; } = 1.0f;
     public bool RandomizeWaypointsDebug { get; set; } = false;
     public Dictionary<uint, Vector3> CrafterLocations { get; set; } = new();
     public List<MissionCommand> PostMissionCommands { get; set; } = new();
